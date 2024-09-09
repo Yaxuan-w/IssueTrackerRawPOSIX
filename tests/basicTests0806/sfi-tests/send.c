92517aa1b23440ce2a75fa5dba7140b20f2230e4
Alice W. <wenyaxuan0925@outlook.com>
2024-09-09 12:54:45 -0400

    add send() test case

---
+++ tests/basicTests0806/sfi-tests/send.c
@@ -0,0 +1,63 @@
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <sys/socket.h>
+
+#define LOOP_COUNT 1000000
+
+long long gettimens() {
+    struct timespec tp;
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    return (long long)tp.tv_sec * 1000000000LL + tp.tv_nsec;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <buffer_size>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int buf_size = atoi(argv[1]);
+    char *buffer = (char *)malloc(buf_size);
+    if (!buffer) {
+        perror("Failed to allocate buffer");
+        exit(EXIT_FAILURE);
+    }
+    memset(buffer, 'A', buf_size);
+
+    // Create a dummy socket
+    int sockfd[2];
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockfd) == -1) {
+        perror("socketpair");
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    long long start_time = gettimens();
+
+    for (int i = 0; i < LOOP_COUNT; i++) {
+        if (send(sockfd[0], buffer, buf_size, 0) == -1) {
+            perror("send");
+            free(buffer);
+            close(sockfd[0]);
+            close(sockfd[1]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    long long end_time = gettimens();
+    long long total_time = end_time - start_time;
+    long long average_time = total_time / LOOP_COUNT;
+
+    fprintf(stderr, "\nBuffer size %d bytes: %d send() calls, average time %lld ns\n", buf_size, LOOP_COUNT, average_time);
+    fflush(NULL);
+
+    free(buffer);
+    close(sockfd[0]);
+    close(sockfd[1]);
+
+    return 0;
+}
